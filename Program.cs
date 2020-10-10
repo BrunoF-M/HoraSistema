12c9e79635dabe70ef0bc882cab2cd7ef0fe3f3c
BrunoF-M <bruno.freitas.marmol77.bm@gmail.com>
2020-10-09 23:56:32 -0300

    Resolução do exercício

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HoraSistema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("=+= Horário atual do Sistema =+=");
+            Console.ResetColor();
+
+            Console.WriteLine();
+
+            DateTime horario = DateTime.Now;
+
+            Console.WriteLine($"Hora do Sistema: {horario}");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Pressione qualquer tecla para Finaliza...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
